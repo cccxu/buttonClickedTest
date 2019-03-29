774f434e6a6a00f0a40f79883f90c790b06e563b
cccxu <xuhaodev@outlook.com>
2019-03-29 20:15:59 +0800

    init

---
+++ app/src/androidTest/java/com/example/buttonclicktest/MainActivityTest.java
@@ -0,0 +1,53 @@
+package com.example.buttonclicktest;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.rule.ActivityTestRule;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+    private MainActivity mainActivity = null;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = activityTestRule.getActivity();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+        Log.d("TAG", "onCreate()");
+    }
+
+    @Test
+    public void btn_click() {
+        Log.d("TAG", "Button was clicked");
+    }
+
+    @Test
+    public void testClickedButton(){
+        mainActivity.findViewById(R.id.button);
+        Espresso.onView(withId(R.id.button)).perform(click());
+        //Espresso.onView(withId(R.id.textView)).perform();
+//        activityTestRule.launchActivity(null);
+//        TextView textView = activityTestRule.getActivity().findViewById(R.id.textView);
+//        String text = textView.getText().toString();
+//        Toast.makeText(mainActivity, text, Toast.LENGTH_LONG).show();
+    }
+}
